34195e27dfe6966965ab0bd4889c4dda3b188802
Steve Kite <korshakov.stepan@gmail.com>
2017-12-10 13:40:22 +0300

    Added pip installing

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 apt-get update -y
-apt-get install -y python3 wget curl
-apt-get install python3-setuptools
-easy_install3 pip
+apt-get install -y python3 wget curl python3-pip
 python3 -m pip install quilt
 rm -fr tmp
