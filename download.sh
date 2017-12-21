859226497a15a4999987f543d23b6e191648327e
Steve Kite <korshakov.stepan@gmail.com>
2017-12-21 18:54:25 +0300

    Dumb change

--- download.sh
+++ download.sh
@@ -8,4 +8,5 @@ mkdir -p downloads
 # wget -O "./tmp/Building_Permits.csv" "https://storage.googleapis.com/files-statecraft/Building_Permits_2017_08_02.csv"
 # wget -O "./tmp/Building_Permits.csv" "https://storage.googleapis.com/files-statecraft/Building_Permits_2017_12_10.csv"
 wget -O "./tmp/Building_Permits.csv" "https://storage.googleapis.com/files-statecraft/Building_Permits_2017_12_15.csv"
+
 mv ./tmp/Building_Permits.csv ./downloads/Building_Permits.csv
